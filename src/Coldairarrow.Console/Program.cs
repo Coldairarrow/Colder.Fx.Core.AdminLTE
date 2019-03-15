827561efec03344e12189914355a5984beb503e8
Coldairarrow <xgc15657519397@outlook.com>
2019-03-15 18:47:15 +0800

    更新

--- src/Coldairarrow.Console/Program.cs
+++ src/Coldairarrow.Console/Program.cs
@@ -11,21 +11,6 @@ namespace Coldairarrow.ConsoleApp
     {
         static void Main(string[] args)
         {
-            IRepository db = DbFactory.GetRepository();
-            db.HandleSqlLog = log =>
-            {
-                Console.WriteLine(log);
-            };
-            Expression<Func<Base_SysLog, Base_User, Base_SysLogDTO>> select = (a, b) => new Base_SysLogDTO
-            {
-                RealName=b.RealName
-            };
-            select = select.BuildExtendSelectExpre();
-            var q = from a in db.GetIQueryable<Base_SysLog>().AsExpandable()
-                    join b in db.GetIQueryable<Base_User>() on a.OpUserName equals b.UserName
-                    select @select.Invoke(a, b);
-
-            var list = q.GetPagination(new Pagination()).ToList();
 
             Console.ReadLine();
         }
