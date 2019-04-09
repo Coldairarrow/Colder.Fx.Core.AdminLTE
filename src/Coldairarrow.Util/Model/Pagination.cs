e6f52c9fbec118313dddeadb2487543161cb161c
Coldairarrow <xgc15657519397@outlook.com>
2019-04-09 15:19:01 +0800

    分页优化

--- src/Coldairarrow.Util/Model/Pagination.cs
+++ src/Coldairarrow.Util/Model/Pagination.cs
@@ -218,6 +218,8 @@ namespace Coldairarrow.Util
 
         #region BootstrapTable方案
 
+        public string sortName { get => _sortField; set => _sortField = value; }
+        public string sortOrder { get => _sortType; set => _sortType = value; }
         public int pageNumber { get => _pageIndex; set => _pageIndex = value; }
         public int pageSize { get => _pageRows; set => _pageRows = value; }
         public object BuildTableResult_BootstrapTable(object dataList)
