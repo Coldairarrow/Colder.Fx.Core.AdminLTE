287fa4b75594207f3a4f7b331f5edb36986c52e7
Coldairarrow <xgc15657519397@outlook.com>
2019-06-14 17:01:13 +0800

    优化

---
+++ src/Coldairarrow.Web/App_Start/00Filter/开启Session/UseSessionAttribute.cs
@@ -0,0 +1,34 @@
+﻿using Coldairarrow.Util;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+
+namespace Coldairarrow.Web
+{
+    /// <summary>
+    /// 使用Session
+    /// </summary>
+    public class UseSessionAttribute : Attribute, IActionFilter, IOrderedFilter
+    {
+        public int Order => 0;
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            var sessionCookie = context.HttpContext.Request.Cookies[SessionHelper.SessionCookieName];
+            if (sessionCookie.IsNullOrEmpty())
+            {
+                string sessionId = Guid.NewGuid().ToString();
+                context.HttpContext.Response.Cookies.Append(SessionHelper.SessionCookieName, sessionId, new CookieOptions { Expires = DateTime.MaxValue });
+
+                context.Result = new RedirectResult(context.HttpContext.Request.GetDisplayUrl());
+            }
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+
+        }
+    }
+}
