73b49bac7482ecd5b24e592f33e20c51983fb5df
Coldairarrow <xgc15657519397@outlook.com>
2019-06-26 22:04:32 +0800

    更新

--- src/Coldairarrow.Web/Controllers/TestController.cs
+++ src/Coldairarrow.Web/Controllers/TestController.cs
@@ -3,28 +3,11 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Coldairarrow.Web
 {
-    //[CheckUrlPermission]
     public class TestController : BaseController
     {
         public ActionResult Index()
         {
             return View();
-        }
-
-        public ActionResult RequestDemo()
-        {
-            return View();
-        }
-
-        public IActionResult Test(string name,int age)
-        {
-            return Content(new { name, age }.ToJson());
-        }
-
-        [CheckSign]
-        public ActionResult RequestTest()
-        {
-            return Success();
         }
     }
 }
