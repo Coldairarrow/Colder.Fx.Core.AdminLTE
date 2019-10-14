2fb48510f8be96d5ef8904cb808e6e896a1ea407
Coldairarrow <xgc15657519397@outlook.com>
2019-10-14 14:34:50 +0800

    优化

--- src/Coldairarrow.Util/Helper/RandomHelper.cs
+++ src/Coldairarrow.Util/Helper/RandomHelper.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -10,7 +9,7 @@ namespace Coldairarrow.Util
     /// </summary>
     public static class RandomHelper
     {
-        private static Random _random { get; } = new Random();
+        private static readonly Random _random  = new Random();
 
         /// <summary>
         /// 下一个随机数
@@ -32,6 +31,42 @@ namespace Coldairarrow.Util
         public static T Next<T>(IEnumerable<T> source)
         {
             return source.ToList()[Next(0, source.Count())];
+        }
+
+        /// <summary>
+        /// 获取不重复的随机值集合
+        /// </summary>
+        /// <typeparam name="T">类型</typeparam>
+        /// <param name="source">数据源</param>
+        /// <param name="count">数量</param>
+        /// <returns></returns>
+        public static List<T> GetValuesNoRepeat<T>(IEnumerable<T> source, int count)
+        {
+            if (source.Count() < count)
+                throw new Exception("选择数量不能大于集合数量!");
+
+            var _source = new List<T>(source).Distinct().ToList();
+            List<T> resList = new List<T>();
+            for (int i = 0; i < count; i++)
+            {
+                resList.Add(GetNewValue());
+            }
+
+            return resList;
+
+            T GetNewValue()
+            {
+                while (true)
+                {
+                    var theValue = Next(_source);
+                    if (!resList.Any(x => x.ToJson() == theValue.ToJson()))
+                    {
+                        _source.Remove(theValue);
+
+                        return theValue;
+                    }
+                }
+            }
         }
     }
 }
