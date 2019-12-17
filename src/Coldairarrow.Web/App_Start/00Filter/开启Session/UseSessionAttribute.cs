41d58ab2329ddc5b3ca90f3124e5d9fee1a38473
Coldairarrow <xgc15657519397@outlook.com>
2019-12-17 21:49:42 +0800

    自定义Session优化，取消重定向

--- src/Coldairarrow.Web/App_Start/00Filter/开启Session/UseSessionAttribute.cs
+++ src/Coldairarrow.Web/App_Start/00Filter/开启Session/UseSessionAttribute.cs
@@ -1,9 +1,8 @@
 ﻿using Coldairarrow.Util;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Http.Extensions;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using System;
+using System.Collections.Generic;
 
 namespace Coldairarrow.Web
 {
@@ -14,13 +13,19 @@ namespace Coldairarrow.Web
     {
         public void OnActionExecuting(ActionExecutingContext context)
         {
+            var requestCookies = context.HttpContext.Request.Cookies.GetPropertyValue("Store") as Dictionary<string, string>;
+            if (requestCookies == null)
+            {
+                requestCookies = new Dictionary<string, string>();
+                context.HttpContext.Request.Cookies.SetPropertyValue("Store", requestCookies);
+            }
+
             var sessionCookie = context.HttpContext.Request.Cookies[SessionHelper.SessionCookieName];
             if (sessionCookie.IsNullOrEmpty())
             {
                 string sessionId = Guid.NewGuid().ToString();
+                requestCookies.Add(SessionHelper.SessionCookieName, sessionId);
                 context.HttpContext.Response.Cookies.Append(SessionHelper.SessionCookieName, sessionId, new CookieOptions { Expires = DateTime.MaxValue, SameSite = SameSiteMode.None });
-
-                context.Result = new RedirectResult(context.HttpContext.Request.GetDisplayUrl());
             }
         }
 
